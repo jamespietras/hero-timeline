baf57e5ac177247988fda0ed0c334787770a723c
James Pietras <james.m.pietras@gmail.com>
2017-05-27 01:10:16 +0200

    Marker popover

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,7 +1,8 @@
+import cx from 'classnames';
 import FontAwesome from 'react-fontawesome';
 import moment from 'moment';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Marker.css';
 
@@ -14,12 +15,48 @@ const propTypes = {
   offset: PropTypes.number.isRequired,
 };
 
-function Marker(props) {
-  return (
-    <div className="marker" style={{ left: `${props.offset * 100}%` }}>
-      <FontAwesome className="marker__icon" name={props.event.icon} />
-    </div>
-  );
+class Marker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      popoverVisible: false,
+    };
+
+    this.changePopoverVisibility = this.changePopoverVisibility.bind(this);
+  }
+
+  changePopoverVisibility(newValue) {
+    this.setState({ popoverVisible: newValue });
+  }
+
+  render() {
+    return (
+      <div
+        className="marker"
+        onMouseEnter={() => this.changePopoverVisibility(true)}
+        onMouseLeave={() => this.changePopoverVisibility(false)}
+        style={{ left: `${this.props.offset * 100}%` }}
+      >
+        <FontAwesome className="marker__icon" name={this.props.event.icon} />
+
+        <div
+          className={cx({
+            marker__popover: true,
+            'marker__popover--open': this.state.popoverVisible,
+          })}
+        >
+          <strong className="marker__popover-date">
+            {this.props.event.date.format('DD.MM.YYYY')}
+          </strong>
+
+          <p className="marker__popover-name">
+            {this.props.event.name}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 Marker.propTypes = propTypes;
